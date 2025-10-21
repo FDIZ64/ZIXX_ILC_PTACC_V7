21678b43bef3265f6a517d1d5c5f08e3f2d712ce
Fabian Diz <fdiz@artech-consulting.com>
2025-10-21 22:47:57 +0000

    UNO

---
+++ webapp/ext/controller/ListReportExtension.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    "sap/m/MessageToast"
+], function(MessageToast) {
+    'use strict';
+
+    return {
+        Edit: function(oEvent) {
+            MessageToast.show("Custom handler invoked.");
+        },
+        onEdit: function(oEvent) {
+            MessageToast.show("editor invoked.");
+
+
+        }
+    }
+});
